6e061969a6ae989ab043e541c8cebb1830c3ce29
Christian Cruz <christian.cruz92@hotmail.com>
2023-12-28 22:29:41 -0500

    added validation to form

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, SelectField
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField("Pet Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
-    age = IntegerField("Age")
-    Notes = StringField("Notes")
+    name = StringField("Pet Name", validators=[InputRequired()])
+    species = SelectField("Species", choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
+    Notes = StringField("Notes", validators=[Optional()])
