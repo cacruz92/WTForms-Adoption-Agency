0baeab63c16634ef21fa898011736e6c1783f734
Christian Cruz <christian.cruz92@hotmail.com>
2023-12-28 23:02:00 -0500

    fixed some issues to allow add route to run

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
+from flask_sqlalchemy import SQLAlchemy
+from forms import AddPetForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt'
@@ -12,11 +14,14 @@ connect_db(app)
 app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
+with app.app_context():
+    db.create_all()
+
 @app.route('/')
 def show_homepage():
     """Shows homepage with list of pet names, photos and availability"""
     pets = Pet.query.all()
-    return render_template(pet_list.html, pets=pets)
+    return render_template('pet_list.html', pets=pets)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pet():
--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
